b326b5eeea5a555c21c0f6bd97b9e6cbbc661db4
Jatin Bishla <bishlajatin@gmail.com>
2022-11-09 01:03:20 +0530

    Create download.py
    External library for downloading csv file

---
+++ download.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+class Download:
+    
+    bold_start = "\033[1m"
+    bold_end = "\033[0:0m"
+
+    def __init__(self, data):
+        self.data = data
+    
+    #main Function to download CSV file
+    def download(self):
+        toBeDownload = {}
+
+        for column in self.data.columns.values:
+            toBeDownload[column] = self.data[column]
+        
+        newFileName = input("\nEnter the" + self.bold_start + "FILENAME" + self.bold_end + "you want:(or press -1 to go back): ")
+
+        if newFileName == -1:
+            return
+        
+        newFileName = newFileName + ".csv"
+        pd.DataFrame(self.data).to_csv(newFileName, index = False) #index=False so that a new column is not added
+
+        print("DONE....\U0001F601")
+
+        if input("Do you want to exit now?(y/n): ") == 'y':
+            print("exiting.... \U0001F44B")
+            exit()
+
+        else:
+            return
